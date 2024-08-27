328ed5a9eb81b0889e828e453bf74060a3a8df49
rajatks1902 <rajatsing4321@gmail.com>
2024-08-27 19:01:11 +0530

    init springBoot Web

---
+++ src/test/java/com/rks07/SpringBootWeb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rks07.SpringBootWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
